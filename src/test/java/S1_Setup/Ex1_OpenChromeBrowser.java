5ff2b2fdfa7844cdf062ac2b23b849c40585a45c
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-07-24 20:07:42 +0530

    24th july 2026

---
+++ src/test/java/S1_Setup/Ex1_OpenChromeBrowser.java
@@ -0,0 +1,15 @@
+package S1_Setup;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Ex1_OpenChromeBrowser
+{
+    public static void main(String[] args)
+    {
+//        ChromeDriver driver=new ChromeDriver();
+//        EdgeDriver driver=new EdgeDriver();
+        FirefoxDriver driver=new FirefoxDriver();
+    }
+}
