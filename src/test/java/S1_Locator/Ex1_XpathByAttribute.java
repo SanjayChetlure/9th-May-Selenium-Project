57b74ad3ea2f5a83b04b76799dc8c89baaa73be9
SanjayChetlure <sanjay.chetlure@gmail.com>
2026-08-04 20:29:31 +0530

    4th Aug 2026

---
+++ src/test/java/S1_Locator/Ex1_XpathByAttribute.java
@@ -0,0 +1,27 @@
+package S1_Locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex1_XpathByAttribute
+{
+    public static void main(String[] args)
+    {
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://testautomationpractice.blogspot.com/");
+
+        //Enter Name
+//       driver.findElement(Locator Type)
+//       driver.findElement(By.xpath(String xpathExpression))
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("abc");
+
+        //Enter Email
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("xyz");
+
+        //Enter Phone Num
+        driver.findElement(By.xpath("//input[@id='phone']")).sendKeys("9999999999");
+
+    }
+}
